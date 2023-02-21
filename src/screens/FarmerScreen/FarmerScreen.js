11d64b375fee0da6343bf20b4c33e3379293db2a
musekwa <musekwa2011@gmail.com>
2023-02-21 05:31:09 +0200

    farmerscreen

--- src/screens/FarmerScreen/FarmerScreen.js
+++ src/screens/FarmerScreen/FarmerScreen.js
@@ -20,66 +20,67 @@ export default function FarmerScreen ({ route, navigation }) {
     const ownerId = route.params.ownerId;
     const realm = useRealm();
     const user = useUser();
-    const customUserData = user?.customData;
+    // const customUserData = user?.customData;
     const farmer = useObject('Farmer', ownerId);
     const farmlands = useQuery('Farmland').filtered('farmer == $0', ownerId);
-    const [fileData, setFileData] = useState(null);
-    const [fileUri, setFileUri] = useState(null);
+    // const [fileData, setFileData] = useState(null);
+    // const [fileUri, setFileUri] = useState(null);
     const [isAddPhoto, setIsAddPhoto] = useState(false);
     const [isPhotoModalVisible, setIsPhotoModalVisible] = useState(false);
 
+    console.log('ownerId:', ownerId);
 
-    const launchNativeCamera = () => {
-      let options = {
-        includeBase64: true,
-        storageOptions: {
-          skipBackup: true,
-          path: 'images',
-        },
-      };
-      launchCamera(options, (response) => {
-        console.log('Response = ', response);
+    // const launchNativeCamera = () => {
+    //   let options = {
+    //     includeBase64: true,
+    //     storageOptions: {
+    //       skipBackup: true,
+    //       path: 'images',
+    //     },
+    //   };
+    //   launchCamera(options, (response) => {
+    //     console.log('Response = ', response);
   
-        if (response.didCancel) {
-          console.log('User cancelled image picker');
-        } else if (response.errorCode) {
-          console.log('ImagePicker Error: ', response.errorMessage);
-        } else {
-          const source = { uri: response.uri };
-          console.log('response', JSON.stringify(response));
-          setFileData(response.assets[0].base64);
-          setFileUri(response.assets[0].uri)
-        }
-      });
+    //     if (response.didCancel) {
+    //       console.log('User cancelled image picker');
+    //     } else if (response.errorCode) {
+    //       console.log('ImagePicker Error: ', response.errorMessage);
+    //     } else {
+    //       const source = { uri: response.uri };
+    //       console.log('response', JSON.stringify(response));
+    //       setFileData(response.assets[0].base64);
+    //       setFileUri(response.assets[0].uri)
+    //     }
+    //   });
   
-    };
+    // };
 
-    const launchNativeImageLibrary = () => {
-      let options = {
-        includeBase64: true,
-        storageOptions: {
-          skipBackup: true,
-          path: 'images',
-        },
-      };
-      launchImageLibrary(options, (response) => {
-        // console.log('Response = ', response);
+    // const launchNativeImageLibrary = () => {
+    //   let options = {
+    //     includeBase64: true,
+    //     storageOptions: {
+    //       skipBackup: true,
+    //       path: 'images',
+    //     },
+    //   };
+    //   launchImageLibrary(options, (response) => {
+    //     // console.log('Response = ', response);
   
-        if (response.didCancel) {
-          console.log('User cancelled image picker');
-        } else if (response.errorCode) {
-          console.log('ImagePicker Error: ', response.error);
-        } else {
-          const source = { uri: response.assets.uri };
-          // console.log('response', JSON.stringify(response));
-          realm.write(()=>{
-            farmer.image = 'data:image/jpeg;base64,' + response.assets[0].base64;
-          })
-          setFileData(response.assets[0].base64);
-          setFileUri(response.assets[0].uri)
-        }
-      });  
-    }
+    //     if (response.didCancel) {
+    //       console.log('User cancelled image picker');
+    //     } else if (response.errorCode) {
+    //       console.log('ImagePicker Error: ', response.error);
+    //     } else {
+    //       const source = { uri: response.assets.uri };
+    //       // console.log('response', JSON.stringify(response));
+    //       realm.write(()=>{
+    //         farmer.image = 'data:image/jpeg;base64,' + response.assets[0].base64;
+    //       })
+    //       setFileData(response.assets[0].base64);
+    //       setFileUri(response.assets[0].uri)
+    //     }
+    //   });  
+    // }
 
 
     return (
